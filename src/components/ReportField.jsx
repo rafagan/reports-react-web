382b3a9989052d92b08f380e2e7ab412a6d1a12a
Ráfagan Abreu <rafagansa@outlook.com>
2021-10-16 14:37:14 -0300

    First component

---
+++ src/components/ReportField.jsx
@@ -0,0 +1,12 @@
+import './ReportField.css'
+
+function ReportField(props) {
+  return (
+    <div className="report-field">
+      <h3>Usuários</h3>
+      <h1>13 mil</h1>
+    </div>
+  );
+}
+
+export default ReportField;
